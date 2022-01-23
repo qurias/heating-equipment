aff6de113de2a964a4b557de1178080cacb04c99
qurias <qurias_it@mail.ru>
2022-01-23 19:27:08 +0300

    65%

--- js/script.js
+++ js/script.js
@@ -12,3 +12,58 @@ $('.filter__item--title').click((elem) => {
       }
   });
 });
+
+
+let slider = document.getElementById('sliderPrice');
+
+noUiSlider.create(slider, {
+  range: {
+    min: 0,
+    max: 99999,
+  },
+  step: 1000,
+
+  start: [0, 999999],
+  connect: true,
+  behaviour: 'tap-drag',
+  tooltips: true,
+});
+
+
+document.querySelectorAll('.filter__item--meaning-circle').forEach((elem) => {
+  elem.addEventListener('click', (elem) => {
+    if(elem.target.dataset.selected == "yes") {
+      elem.target.dataset.selected = "no"
+      elem.target.style.color = '#7E838C';
+      elem.target.classList.remove('selectedColor');
+      let countSelected = 0; 
+      document
+        .querySelectorAll('.filter__item--meaning-circle')
+        .forEach((elem) => {
+          
+          elem.dataset.selected == 'yes'
+            ? countSelected += 1
+            : '';
+        });
+        if(countSelected == 0) {
+          document
+            .querySelectorAll('.filter__item--meaning-circle')
+            .forEach((elem) => {
+              elem.style.color = 'black'
+            });
+        }
+    }
+    else {
+      elem.target.classList.add('selectedColor');
+      elem.target.style.color = 'black';
+      elem.target.dataset.selected = 'yes';
+      document
+        .querySelectorAll('.filter__item--meaning-circle')
+        .forEach((elem) => {
+          elem.dataset.selected == 'yes'
+            ? ''
+            : (elem.style.color = '#7E838C');
+        });
+    }
+  });
+});
